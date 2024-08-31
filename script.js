01500d6b94f28944112520551d180b76ffd225a9
Anushka Dhakar <anushkadhakar442@gmail.com>
2024-08-31 20:02:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const dropdownMenu = document.querySelector('.dropdown_menu');
 
-// Add a click event listener to the hamburger icon
 hamburgerIcon.addEventListener('click', function() {
   dropdownMenu.classList.toggle('show');
 });
